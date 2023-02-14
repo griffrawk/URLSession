ef8c629679079d45c05e5deb5fd8689ad6203377
Andy Griffiths <griffiths_andy@icloud.com>
2023-02-14 10:24:44 +0000

    Initial Commit

---
+++ URLSession/URLSessionApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLSessionApp.swift
+//  URLSession
+//
+//  Created by Andy Griffiths on 14/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct URLSessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
