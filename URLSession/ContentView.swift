516aeece7d8805614ce193f0c16104fd7cacb196
Andy Griffiths <griffiths_andy@icloud.com>
2023-02-14 11:32:45 +0000

    Brief experiment with async / await

--- URLSession/ContentView.swift
+++ URLSession/ContentView.swift
@@ -7,15 +7,69 @@
 
 import SwiftUI
 
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+}
+
 struct ContentView: View {
+    @State private var results = [Result]()
+    @State private var message = "Loading..."
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text(message)
+                .task {
+                    do {
+                        // I think I understand async / await a bit better now.
+                        // .sleep is marked async, so has to be called await
+                        // and because also marked as throws, has to be 'try'ed.
+                        // do {} catch {} is necessary to catch the possible throw
+                        // even if I do nothing with an error.
+                        try await Task.sleep(until: .now + .seconds(3), clock: .continuous)
+                    } catch { }
+                    message = "Woken up"
+                    
+                    // ofc I can convert any error to an optional, or could disable with try!
+                    try? await Task.sleep(until: .now + .seconds(3), clock: .continuous)
+                    message = "Had at least 2 cups of coffee"
+                }
+            List(results, id: \.trackId) { item in
+                VStack(alignment: .leading) {
+                    Text(item.trackName)
+                        .font(.headline)
+                    Text(item.collectionName)
+                }
+            }
+            .task {
+                // similarly I have marked loadData as async (but not throws)
+                // so just await will suffice here
+                await loadData()
+            }
         }
-        .padding()
+    }
+    
+    func loadData() async {
+        // loadData has to be marked async because it uses await calls
+        guard let url = URL(string: "https://itunes.apple.com/search?term=electric+wizard&entity=song") else {
+            print("Invalid URL")
+            return
+        }
+        
+        do {
+            // Again .data is async throws, so try await in do catch
+            let (data, _) = try await URLSession.shared.data(from: url)
+            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                results = decodedResponse.results
+            }
+        } catch {
+            print("Invalid data")
+        }
     }
 }
 
